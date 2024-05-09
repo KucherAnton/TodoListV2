9bfb5ee0189e1c1d57ebf3031f617a4db10188bc
Антон Кучер <kapetrovi4@gmail.com>
2024-05-09 05:35:44 +0300

    init of nextjs, configuration of docker, base for user db model

---
+++ backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo:27017/Cluster0'),
+    UserModule,
+  ],
+})
+export class AppModule {}
